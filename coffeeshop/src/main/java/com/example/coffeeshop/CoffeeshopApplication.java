10a0cb366b3cebdc14d306681e2f950278a63cd6
Nattha-nan <natthanan.bo@kkumail.com>
2026-07-22 14:04:50 +0700

    code1

--- coffeeshop/src/main/java/com/example/coffeeshop/CoffeeshopApplication.java
+++ coffeeshop/src/main/java/com/example/coffeeshop/CoffeeshopApplication.java
@@ -1,4 +1,4 @@
-package com.example.lab5;
+package com.example.coffeeshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
